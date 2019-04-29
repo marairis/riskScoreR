71a537aff7d4e513400a28d1e35778511abdc26c
Mara Tews <temai2606@gmail.com>
2019-04-29 12:48:19 +0200

    target function is now deviance

--- R/penalty_function.R
+++ R/penalty_function.R
@@ -51,11 +51,10 @@ optimized_measure <- function(a, balance, data, importance, k, grid.res, weight,
     }
   }
   # make sure that feature.names and importance/training/testing column names match
-  ## mit "x1" nicht ideal, allgemeine abfrage fuer x/snp?
-  if("x1" %in% feature.names) {
+  if(charmatch("x", feature.names) != "nomatch") {
     colnames(data) <- c(paste("x", (1:(ncol(data)-1)), sep = ""), y.name)
     names(importance) <-  paste("x", (1:(ncol(data)-1)), sep = "")
-  } else if ("snp1" %in% feature.names) {
+  } else if (charmatch("snp", feature.names) != "nomatch") {
     colnames(data) <- c(paste("snp", (1:(ncol(data)-1)), sep = ""), y.name)
     names(importance) <-  paste("snp", (1:(ncol(data)-1)), sep = "")
   } 
@@ -64,92 +63,75 @@ optimized_measure <- function(a, balance, data, importance, k, grid.res, weight,
   penalty <- function(a, w, balance, data) {
     step <- ceiling(log(((w/a)+2)/2, 1.5))
     current_penalty <- step*balance
-    # transform into [0,1] based on maximum number of w
-    max_penalty <- (ceiling(log((((ncol(data)-1)/a)+2)/2, 1.5)))*balance
-    penalty2auc <- current_penalty/max_penalty
-    return(penalty2auc)
+    return(current_penalty)
   }
 
-  # (ii) get ready for cross-validation
-  n_data <- nrow(data)
-  # ranking snps based on impotance (from gwas)
-  variables_ranked <- sort(importance, decreasing = TRUE)
-  # shuffle data for crossvalidation
-  perm_idx <- sample(nrow(data))
-  # calculate possible values for w
-  w_candidates <- sapply(list(1:grid.res), function(s) ((ncol(data)-1)/grid.res)*s)
-  cv_auc_penalized <- double(grid.res)
+  # (ii) initilize optimization problem (min)
+  resp_matrix <- matrix(0, nrow = nrow(data), ncol = length(importance))
   
-  # describe modelling function: does a cross-validation and returns performance for current w
-  modelling <- function(j) {
-    cv_auc_values <- double(k)
-    for (i in 0:(k-1)) {
-      # select train and test data
-      cv_test <- data[perm_idx[((n_data/k)*i+1):((n_data/k)*(i+1))], ]
-      cv_train <- data[-perm_idx[((n_data/k)*i+1):((n_data/k)*(i+1))], ]
-
-      # set up model
-      # create parameters adjusted to w
-      #cv_formula <- as.formula(paste("state ~", paste(paste0("x", 1:w_candidates[j]), collapse = "+")))
-      cv_y.name <- y.name
-      ### in var.ranked: x statt snp, da spaltenname x in importance?
-      cv_feature.names <- names(variables_ranked[1:w_candidates[j]])
-      cv_importance <- variables_ranked[1:w_candidates[j]]
-      cv_data <- dplyr::select(cv_train, cv_feature.names, y.name)
-      
-      cv_model <- riskScorer(data = cv_data,
-                             y.name = cv_y.name,
-                             feature.names = cv_feature.names,
-                             importance = cv_importance,
-                             weight = weight,
-                             beta = TRUE)
-
-      # validate on test data
-      cv_newdata <- dplyr::select(cv_test, cv_feature.names, y.name)
-      cv_prediction <- predict.riskScorer(risk.scorer = cv_model,
-                                          newdata = cv_newdata,
-                                          type = "prob")
-
-      # get auc
-      ## welche spalte uebergeben?
-      cv_auc_values[(i+1)] <- mlr::measureAUC(probabilities = cv_prediction$`1`,
-                                              truth = cv_test[, y.name],
-                                              negative = 1,
-                                              positive = 0)
-    }
+  dev_modelling <- function(w) {
+    # true/false matrix, size depending on #snps = w
+    tf_matrix <- matrix(1, nrow = w, ncol = w, byrow = TRUE)
+    tf_matrix[lower.tri(tf_matrix)] <- 0
+    # importance with size depending on w
+    dev_importance <- sort(importance, decreasing = TRUE)[w]
+    # data with size depending on w
+    dev_data <- dplyr::select(data, names(dev_importance), y.name)
+  
+    # multiply true/false matrix with importance
+    temp <- tf_matrix*dev_importance
+  
+    # response matrix
+    resp_model <- riskScorer(data = dev_data,
+                                   y.name = "state",
+                                   feature.names = names(dev_importance),
+                                   importance = dev_importance,
+                                   weight = TRUE,
+                                   beta = TRUE)
     
-    # substract current penalty from averaged auc
-    ## wenn auc < 0.5: 1-auc, dh modell umdrehen, wie?
-    cv_penalty <- penalty(a = a,
-                          w = w_candidates[j], 
-                          balance = balance, 
-                          data = data)
-    cv_auc_penalized[j] <- mean(cv_auc_values) - cv_penalty
-    if (cv_auc_penalized[j] < 0) {
-      cv_auc_penalized[j] <- 0
-    } 
-    message("Current number of SNPs:", w_candidates[j],"  Averaged AUC:", cv_auc_penalized[j], " (Penalty:", cv_penalty, ")")
-    return(cv_auc_penalized[j])
+    # save response in matrix // einfach mit trainingsdaten predicten? 
+    resp_values <- predict.riskScorer(resp_model,
+                                      dev_data[1:(length(dev_importance))],
+                                      "response")
+    
+    resp_matrix[,w] <- unlist(resp_values)
   }
-  # get all aucs as an vector (vlt besser dataframe? mit anzahl snps, auc, penalty?)
-  get_models <- sapply(X = 1:grid.res,
-                       FUN = modelling)
   
-  # noch machen: auc > 1, ausgabe finales modell -> auswahl aus get_models
-
-  # select max auc
-  idx_w <- which.max(get_models)
-  message("Best result for w = " ,w_candidates[idx_w], " (AUC = ", get_models[idx_w], ")")
-
+  # matrix with all possible responses(w) for all person (n)
+  dev_models <- sapply(X = 1:length(importance), 
+                       FUN = dev_modelling)
+  
+  # get devianz for each model (#models = w) 
+  # nur wert fuer family: gaussian/laplace (egal welcher type predictet wurde) // werte zwischen 0-1 
+  # family: verteilung der zielvariable (siehe plot?)
+  mod_deviance <- matrix(data = sapply(X = 1:(ncol(dev_models)-1),
+                                       FUN = function (s) dismo::calc.deviance(obs = as.numeric(dev_models[,ncol(dev_models)]), 
+                                                                               pred = as.numeric(dev_models[,s]), 
+                                                                               family = "gaussian")),
+                         ncol = ncol(dev_models)-1,
+                         nrow = 1,
+                         byrow = TRUE)
+  
+  # get penalty [auf 0-1 beschraenkt]
+  max_pen <- penalty(a = a, w = length(importance), balance = balance, data = data)
+  mod_penalty <- matrix(data = sapply(X = 1: ncol(mod_deviance),
+                                      FUN = function (s) penalty(w = s, a = a, balance = balance, data = data)/max_pen),
+                        ncol = ncol(mod_deviance),
+                        nrow = 1,
+                        byrow = TRUE)
+  
+  # solve optimization problem: min (devianz + penalty)
+  best_model <- which.min(mod_deviance + mod_penalty)
+  
   # get final model for prediction
-  fin_feature.names <- names(variables_ranked[1:w_candidates[idx_w]]) 
+  fin_importance <- sort(importance, decreasing = TRUE)[best_model]
+  fin_feature.names <- names(fin_importance) 
   fin_y.name <- y.name
-  fin_importance <- variables_ranked[1:w_candidates[idx_w]]
-  fin_data <- dplyr::select(data, fin_feature.names, fin_y.name)
+  fin_data <- dplyr::select(data, names(fin_importance), fin_y.name)
   
   fin_model <- riskScorer(data = fin_data, 
                           y.name = fin_y.name, 
-                          feature.names = fin_feature.names,
+                          feature.names = names(fin_importance),
                           importance = fin_importance,
                           weight = weight, 
                           beta = TRUE)
