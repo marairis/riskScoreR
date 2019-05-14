7a5ed29ff54fc192a41339496c83b3a9ac9fae69
Damian Gola <gola@imbs.uni-luebeck.de>
2019-05-14 11:16:43 +0200

    change variable name from importance_data to importance

--- R/regularizedRiskScorer.R
+++ R/regularizedRiskScorer.R
@@ -23,7 +23,7 @@
 #' @return Optimized number of SNPs w to use in final model.
 #'
 #' @export
-#' 
+#'
 regularizedRiskScorer <- function(a, balance, data, importance, k, grid.res, weight, formula, beta, ...) {
   # (i) check input and set default values
   checkmate::assertNumber(a)
@@ -44,10 +44,10 @@ regularizedRiskScorer <- function(a, balance, data, importance, k, grid.res, wei
       stop("If not providing a formula, 'y.name' and 'feature.names' must be given!")
     }
   }
-  
+
   # sort importance
   imp_sorted <- importance[order(abs(importance), decreasing = TRUE)]
-  
+
   # initialize penalty function
   penalty <- function(a, w, balance) {
     step <- ceiling(log(((w/a)+2)/2, 1.5))
@@ -58,7 +58,7 @@ regularizedRiskScorer <- function(a, balance, data, importance, k, grid.res, wei
   # (ii) initilize optimization problem (min)
   dev_vector <- matrix(0, nrow = length(importance), ncol = 1)
   dev_imp <- rep(0,length(importance))
-  names(dev_imp) <- names(importance_data)
+  names(dev_imp) <- names(importance)
   # get intervall of possible scores
   min_resp <- sum(importance[importance<0])*2
   max_resp <- sum(importance[importance>0])*2
@@ -66,38 +66,38 @@ regularizedRiskScorer <- function(a, balance, data, importance, k, grid.res, wei
   # transform 0/1 to -1/1
   dev_truth <- (2*data[, y.name])-1
   dev_data <- as.matrix(data[, names(importance)])
-  
+
   dev_modelling <- function(w) {
     # only use importance 1:w, other values = 0
     dev_imp[names(imp_sorted[1:w])] <- imp_sorted[1:w]
     # return probability
     resp_temp <- dev_data%*%dev_imp
-    resp_temp <- resp_temp/interval_resp 
+    resp_temp <- resp_temp/interval_resp
     # get devianz for each model
     dev_vector[w] <- sum(-log(1+exp(-2*dev_truth*(2*resp_temp-1))))
   }
   # returns vector with deviance for each model
-  mod_deviance <- sapply(X = 1:length(importance), 
+  mod_deviance <- sapply(X = 1:length(importance),
                        FUN = dev_modelling)
-  
+
   # get penalty
   mod_penalty <- sapply(X = 1: length(mod_deviance),
                         FUN = function (s) penalty(w = s, a = a, balance = balance))
-  
+
   # solve optimization problem: max(devianz - penalty)
   best_model <- which.max(mod_deviance - mod_penalty)
-  
+
   # get final model for prediction
   fin_imp <- imp_sorted[1:best_model]
-  fin_feature.names <- names(fin_imp) 
+  fin_feature.names <- names(fin_imp)
   fin_y.name <- y.name
   fin_data <- dplyr::select(data, fin_feature.names, y.name)
-  
-  fin_model <- riskScorer(data = fin_data, 
-                          y.name = fin_y.name, 
+
+  fin_model <- riskScorer(data = fin_data,
+                          y.name = fin_y.name,
                           feature.names = names(fin_imp),
                           importance = fin_imp,
-                          weight = weight, 
+                          weight = weight,
                           beta = TRUE)
   return(fin_model)
 }
